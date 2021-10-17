2c20ee2a79fcb46adc00370aed47d9dadb96f02c
yq <1045046593@qq.com>
2021-10-17 20:36:08 +0800

    feat: week02 homework

---
+++ week02/degree-of-an-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 1、通过hash表记录每个值出现次数和第一次以及最后一次出现的位置
+ * 2、遍历hash表得到次数最多的值，通过最后一次出现位置和第一次出现位置计算最小长度
+ */
+var findShortestSubArray = function (nums) {
+  const hash = {};
+  let max = 0;
+  let ans = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]]) {
+      hash[nums[i]][0] += 1;
+      hash[nums[i]][2] = i;
+    } else {
+      hash[nums[i]] = [1, i, i];
+    }
+  }
+  for (let key in hash) {
+    const [count, first, last] = hash[key];
+    let length = last - first + 1;
+    if (count > max) {
+      max = count;
+      ans = length;
+    } else if (count === max) {
+      if (ans > length) {
+        ans = length;
+      }
+    }
+  }
+  return ans;
+};
---
+++ week02/subdomain-visit-count.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ * 1、通过hash表统计每个域名的数值
+ * 2、遍历hash表得到结果
+ */
+var subdomainVisits = function (cpdomains) {
+  const sumHash = {};
+  const ans = [];
+  for (let i = 0; i < cpdomains.length; i++) {
+    const [count, domain] = cpdomains[i].split(' ');
+    const domains = domain.split('.');
+    let prev = '';
+    for (let j = domains.length - 1; j > -1; j--) {
+      prev = `${domains[j]}${prev ? `.${prev}` : prev}`;
+      if (sumHash[prev]) {
+        sumHash[prev] += +count;
+      } else {
+        sumHash[prev] = +count;
+      }
+    }
+  }
+  for (let key in sumHash) {
+    ans.push(`${sumHash[key]} ${key}`);
+  }
+  return ans;
+};
